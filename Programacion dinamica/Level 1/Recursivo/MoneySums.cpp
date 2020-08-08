807915be75d0a6f97ab3c46e2c0b9d55970eef37
John Alex <jhon@DESKTOP-FDPKD12.localdomain>
2020-08-08 00:37:40 -0500

    Dia 6

--- Programacion dinamica/Level 1/Recursivo/MoneySums.cpp
+++ Programacion dinamica/Level 1/Recursivo/MoneySums.cpp
@@ -8,18 +8,52 @@ Jhon Alex Gaviria
 
 using namespace std;
 
-int dp[1000];
+int dp[100005][105], n, coin[104], answ[100005];
+
+void solve(int i, int j){
+
+    if (j>n) return; 
+
+    if(dp[i][j] != -1) return; 
+
+    dp[i][j] = 1;
+
+    solve(i+coin[j], j+1);
+
+    solve(i, j+1);
+}
 
 int main () {
 
-    int n, coin[104];
+    int  cont=0;
 
-    cin >> n; 
+    memset(dp, -1, sizeof dp);
+
+    cin >> n;
 
     for(int i= 0; i<n; i++)
         cin >> coin[i];
+
+    solve(0,0);
+
+    for(int i=0; i<100003; i++)
+    {
+        for(int j=0; j<104; j++)
+        {
+            if(dp[i][j] == 1)
+            {
+                answ[cont] = i;
+                cont++;
+                break;
+            }
+        }
+    }
+
+    cout << cont-1 << endl;
+
+    for(int i=1; i<cont; i++)
+        cout << answ[i] << " ";
     
-    
-    for(int i = 0; i<n; i++)
-        for(int j=i; j<n; j++)
+    cout << endl;
+
 }
