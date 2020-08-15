ef57e77f34d979c83aea576130ab8863faf3685a
John Alex <j.gaviria@utp.edu.co>
2020-08-15 04:11:39 -0500

    Dia 12

---
+++ Other Problems/I-FerryLoadingIII.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+int main(){
+
+    int t, n, m, c, aux;
+    string aux2; 
+    map<int, string> ferry; 
+
+    cin  >> c; 
+
+    while(c--){
+
+        cin >> n >> t >> m;
+
+        for(int i=0; i<m; i++){
+            cin >> aux >> aux2;
+            ferry[aux] = aux2;  
+        }
+        
+        
+    }
+}
