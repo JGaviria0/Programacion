2c8301d48be3d466d5aacce57cb74cb21ca139d1
John Alex <j.gaviria@utp.edu.co>
2020-08-18 01:35:31 -0500

    Dia 15

--- Other Problems/Contest 1 VJudge/J-BlackBox.cpp
+++ Other Problems/Contest 1 VJudge/J-BlackBox.cpp
@@ -6,7 +6,7 @@ int main (){
 
     int n, t, m, a[30004], b[30004], aux[30004];
 
-    set<int> q; 
+    priority_queue<int> men, may;
 
     string lineignore; 
     cin >> t; 
@@ -25,24 +25,45 @@ int main (){
         for(int i=0; i<n; i++) 
             cin >> b[i]; 
 
-        int i=0, j=0, k=0; 
+        int i=1, j=0, k=0; 
+        men.push(a[0]); 
+        may.push(-2000000006);
         
         while(i<=m && j<n){
 
             if(i < b[j])
-            {
-                q.insert(a[i]);  
+            {   
+                if( men.top() <= a[i])
+                {
+                    may.push(-a[i]);
+                }  
+                else{
+                    int aux = - may.top(); 
+                    int aux2 = men.top(); 
+                    may.push(-men.top()); 
+                    men.pop();
+                    men.push(a[i]); 
+                    //cout << aux << " <---- " << men.top() <<" ----> " << aux2 << endl; 
+                }
                 i++; 
+                
             }
             else
             {
-                auto it = q.begin(); 
-                cout << *it+i << endl;
+                cout << men.top() << endl; 
+                if(!may.empty()){
+                    men.push(-may.top());
+                    may.pop(); 
+                }
                 k++; 
                 j++; 
             }
         }
         if(t != 0)
             cout << endl;
+        while(!men.empty())
+            men.pop();
+        while(!may.empty())
+            may.pop();
     }
 }
