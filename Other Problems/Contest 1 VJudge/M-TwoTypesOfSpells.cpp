ea0066aa3334bca4a45f824234b6f6465b1723ea
John Alex <j.gaviria@utp.edu.co>
2020-08-19 23:43:51 -0500

    Dia 17

--- Other Problems/Contest 1 VJudge/M-TwoTypesOfSpells.cpp
+++ Other Problems/Contest 1 VJudge/M-TwoTypesOfSpells.cpp
@@ -2,48 +2,35 @@
 
 using namespace std; 
 
-map<string, int> spell; 
-
 int main (){
 
-    int n, tipe, num, numAux;
-    long long sum = 0; 
-    string  numstring, numstringAux;
-    bool flag = false; 
+    int n, k, m, spell, power, KAdd, RestAdd; 
+
+    multiset<int> theK, theRest; 
 
     cin >> n; 
 
-    while(n--){
+    KAdd =  RestAdd =0; 
 
-        cin >> tipe >> num; 
-        numstring = to_string(num); 
-        if(tipe == 0)
-        {
-            if(num > 0){
-                if (flag){
-                    flag = false; 
-                    spell[numstringAux] += num;
-                    sum += num;   
-                } 
-                spell[numstring] = num;
-                sum += num; 
-            }
-            else
-            {
-                
-            }
-        }
-        else
-        {
-            if(num > 0){
-                if (flag){
-                    spell[numstringAux] += num;
-                    sum += num;   
+    while(n--){
+        
+        cin >> spell >> power; 
+
+        if (spell == 0){
+            if(power > 0){
+                if( power > -*theK.begin()){
+                    theRest.insert(- *theK.begin()); 
+                    KAdd -= - *theK.begin();
+                    RestAdd += - *theK.begin();
+                    theK.erase(theK.begin()); 
+                    theK.insert( - power); 
+                    KAdd += power; 
                 }
-                flag = true; 
-                numstringAux = numstring; 
-                spell[numstring] = num;
-                sum += num; 
+                else
+                {
+                    theRest.insert(power); 
+                    RestAdd += power; 
+                }
             }
             else
             {
@@ -51,5 +38,5 @@ int main (){
             }
             
         }
-    }
+    } 
 }
