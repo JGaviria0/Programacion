a575ea60fccb97cbdd68d90028453435e3bc78e2
JGaviria0 <j.gaviria@utp.edu.co>
2020-09-05 21:21:38 -0500

    Dia 34

--- day-to-day competencies/NWERC 2014/K.cpp
+++ day-to-day competencies/NWERC 2014/K.cpp
@@ -6,10 +6,58 @@ int main (){
 
     ios::sync_with_stdio(0); cin.tie(0);
 
-    int n, m, t, pos[10000];
+    int n, s, t, naux, saux, taux;
+    long long ans, total; 
+    map <int, int> equipaje; 
+    map <int, int> aux;
+    pair <int, int > in, out;  
 
-    cin >> n >> m >> t; 
+    cin >> n >> s >> t; 
 
-    
+    for (int i= 0; i<n; i++){
+        cin >> naux; 
+        equipaje[naux]++;  
+    }
 
+    ans = 1e18;  
+
+    for (auto i : equipaje) {
+
+        aux = equipaje;
+        in = i;
+        total = 0;  
+
+        
+
+        
+
+        while(!aux.empty()) {
+
+            out = *aux.lower_bound((in.first + t)%s);
+
+            aux[in.first]--; 
+            if(aux[in.first] <= 0) {
+                aux.erase(aux.find(in.first)); 
+            }
+
+            for(auto j : aux) {
+                cout << j.first << " "; 
+            }
+            cout << endl;
+
+            total += t; 
+            if ((in.first + t)%s > out.first) {
+                total += s - ((in.first + t)%s) + out.first; 
+            } else {
+                total += out.first - ((in.first + t)%s);
+            } 
+            
+            in = out; 
+
+        }
+
+        ans = min(total, ans);
+    }
+
+    cout << ans << endl; 
 }
